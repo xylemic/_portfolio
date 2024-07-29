6955ed825a9e7db5d1bbb0bc1f60b1219e2b2e98
xylemic <junesveryown6ix@gmail.com>
2024-07-29 23:55:51 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,49 @@
+const side_menu = document.getElementById('side_menu');
+const nav_bar = document.querySelector('nav');
+const nav_links = document.querySelector('nav ul');
+
+function open_menu() {
+  side_menu.style.transform = 'translateX(-16rem)';
+}
+
+function close_menu() {
+  side_menu.style.transform = 'translateX(16rem)';
+}
+
+window.addEventListener('scroll', () => {
+  if (scrollY > 50) {
+    nav_bar.classList.add('bg-white', 'bg-opacity-50', 'backdrop-blur-lg', 'shadow-sm');
+
+    nav_links.classList.remove('bg-white', 'shadow-sm', 'bg-opacity-50');
+  } else {
+    nav_bar.classList.remove('bg-white', 'bg-opacity-50', 'backdrop-blur-lg', 'shadow-sm');
+
+    nav_links.classList.add('bg-white', 'shadow-sm', 'bg-opacity-50');
+  }
+})
+
+// light mode and dark mode
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+  document.documentElement.classList.add('dark')
+} else {
+  document.documentElement.classList.remove('dark')
+}
+
+// Whenever the user explicitly chooses light mode
+localStorage.theme = 'light'
+
+// Whenever the user explicitly chooses dark mode
+localStorage.theme = 'dark'
+
+// Whenever the user explicitly chooses to respect the OS preference
+localStorage.removeItem('theme')
+
+function toggle_theme() {
+  console.log('button clicked');
+  document.documentElement.classList.toggle('dark');
+  if (document.documentElement.classList.contains('dark')) {
+    localStorage.theme = 'dark';
+  } else {
+    localStorage.theme = 'light';
+  }
+}
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      fontFamily: {
+      Outfit: ["Outfit", "sans-serif"],
+      Ovo: ["Ovo", "serif"]
+    },
+    animation: {
+      spin_slow: 'spin 6s linear infinite'
+    },
+    colors: {
+      light_hover: '#fcf4ff',
+      dark_hover: '#2a004a',
+      dark_theme: '#11001f'
+    },
+    boxShadow: {
+      'black': '4px 4px 0 #000', 
+      'white': '4px 4px 0 #fff'
+      }
+    }
+  },
+ dark_mode: 'selector'
+}
